03155cf8554ce9f19313f706002179c56cb781ff
Administrator <2544762897@qq.com>
2020-02-24 18:01:58 +0800

    api alright

--- Blue_Cube_Star/Blue_Cube_Star/create_table.py
+++ Blue_Cube_Star/Blue_Cube_Star/create_table.py
@@ -1,10 +1,14 @@
+from some_useful_func import next_id
 import mysql.connector
-conn = mysql.connector.connect(user='MCV', password='raspberrypi', database='MCV_database')
+conn = mysql.connector.connect(user='MCV', password='raspberrypi', database='AtomHeart')
 cursor = conn.cursor()
-# cursor.execute('create table MCV_table (id varchar(500) primary key, server_name varchar(200), level Int, '
-#               'server_args varchar(2000), table_names varchar(20000), addition varchar(10000))')
-cursor.execute('insert into MCV_table (id, server_name, level, server_args, table_names) values '
-               '(%s, %s, %s, %s, %s)', ['0', 'test', 10, 'test_init,write_in,write_out', 'test'])
+cursor.execute('create table  Chemistry_competition (id varchar(500) primary key, text text(65535), level Int, '
+               'image_address varchar(2000), belong_to varchar(20000), addition text(65535))')
+# cursor.execute('create table  Chemistry_competition (id varchar(500) primary key, text text(65535), level Int, '
+#               'image_address varchar(2000), belong_to varchar(20000), addition text(65535))')
+# cursor.execute('insert into MCV_table (id, server_name, level, server_args, table_names) values '
+#               '(%s, %s, %s, %s, %s)', [next_id(), 'AtomHeart', 10, 'atom_heart_init,write_in,',
+#                                        'Chemistry(normal),Chemistry(competition)'])
 conn.commit()
 cursor.close()
 cursor = conn.cursor()
